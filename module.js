d8cd0c1d08feae81490ca5d076697423581895f8
Shivam-githu <stshivam1995@gmail.com>
2021-03-18 13:54:17 +0530

    firstcommit

---
+++ module.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var oops_1 = require("./oops");
+var posObj = new oops_1.Position(101, "Narendra", 40000, "Java Developer", "Banglore");
+posObj.displayDetail();
+var empObj = new oops_1.Employee(201, "Aiswarya", 50000);
+empObj.displayDetail();
